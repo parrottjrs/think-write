d204d9ae54366c226752cb5375b4e916a9b73826
Jordan Parrott <parrottjrs@gmail.com>
2023-06-15 14:19:22 -0230

    more local storage

--- src/components/SaveButton.tsx
+++ src/components/SaveButton.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function SaveButton() {
+export default function SaveButton(note) {
+  const getNotes = () => {
+    const maybeNotes = localStorage.getItem("notes");
+
+    return maybeNotes ? JSON.parse(maybeNotes) : [];
+  };
+
+  let notes = getNotes();
+
+  const saveNote = (note) => {
+    const noteToSave = note;
+
+    notes.unshift(noteToSave);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    localStorage.removeItem("text");
+  };
   return (
     <a
       href="/think-write/save"
-      onClick={() => {
-        console.log("save");
-      }}
+      onClick={() => saveNote(note)}
       className="self-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded m-10"
     >
       Save
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -3,20 +3,34 @@ import { useState, useEffect } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import SaveButton from "../components/SaveButton";
+import { v4 as uuid } from "uuid";
 
 export default function Editor() {
-  const currentNote = JSON.parse(localStorage.getItem("note") ?? "");
+  const getText = () => {
+    const maybeText = localStorage.getItem("text");
 
-  const [note, setNote] = useState(currentNote);
+    if (maybeText === null) {
+      return;
+    } else {
+      return JSON.parse(maybeText);
+    }
+  };
+
+  const currentNote = {
+    text: getText(),
+    id: uuid(),
+  };
+
+  const [text, setText] = useState(currentNote.text);
 
   useEffect(() => {
-    localStorage.setItem("note", JSON.stringify(note));
-  }, [note]);
+    localStorage.setItem("text", JSON.stringify(text));
+  }, [text]);
 
   return (
     <div className="w-100 flex flex-col">
-      <ReactQuill theme="snow" value={note} onChange={setNote} />
-      <SaveButton />
+      <ReactQuill theme="snow" value={text} onChange={setText} />
+      <SaveButton note={currentNote} />
     </div>
   );
 }
