03cc95bd4f637b525e1e52f5221e9f585694cac5
Jordan Parrott <parrottjrs@gmail.com>
2023-08-13 12:58:43 -0230

    Update SessionList.tsx
    Changed past sessions to dropdowns.

--- src/components/SessionList.tsx
+++ src/components/SessionList.tsx
@@ -10,6 +10,7 @@ const SessionListItem = ({ session, id, hot, sessions, title }) => {
 
   const [change, setChange] = useState(true);
   const [sessionText, setSessionText] = useState(cold);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const newSessions = [...sessions];
@@ -31,44 +32,69 @@ const SessionListItem = ({ session, id, hot, sessions, title }) => {
   };
 
   return (
-    <section className="break-words mt-5 md:mt-8">
-      <div className="flex items-center">
+    <Collapsible.Root
+      className="w-full md:w-2/3 self-center mb-5"
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <div className="flex justify-left">
         <p className="text-cyan-300 font-thin tracking-wider text-md md:text-xl mr-4">
           Session {sessionId}
         </p>
-        {lockCheck(unlockDate) ? (
-          <EditButton
-            onClick={() => {
-              setChange(!change);
-            }}
-          />
-        ) : (
-          <p className="text-slate-300 text-xs md:text-md font-thin tracking-wider">
-            Locked until {unlockDate}
-          </p>
-        )}
+        <Collapsible.Trigger asChild>
+          <button id={open ? "close" : "open"}>
+            {open ? (
+              <XCircle
+                className="p-0.5 text-slate-300  hover:text-slate-600"
+                strokeWidth={1}
+              />
+            ) : (
+              <ArrowDownFromLine
+                className="p-0.5 text-slate-300 hover:text-slate-600"
+                strokeWidth={1}
+              />
+            )}
+          </button>
+        </Collapsible.Trigger>
       </div>
-      <div className="my-2 border border-slate-600/25" />
-      {change ? (
-        <div
-          className="my-1 text-slate-300 text-sm md:text-lg font-thin tracking-wider"
-          dangerouslySetInnerHTML={{ __html: cold }}
-        />
-      ) : (
-        <ReactQuill
-          className="text-slate-300"
-          value={sessionText}
-          onChange={handleChange}
-        />
-      )}
-    </section>
+      <Collapsible.Content className="select-none">
+        <section className="break-words mt-5 md:mt-8">
+          <div className="flex items-center">
+            {lockCheck(unlockDate) ? (
+              <EditButton
+                onClick={() => {
+                  setChange(!change);
+                }}
+              />
+            ) : (
+              <p className="text-slate-300 text-xs md:text-md font-thin tracking-wider">
+                Locked until {unlockDate}
+              </p>
+            )}
+          </div>
+          <div className="my-2 border border-slate-600/25" />
+          {change ? (
+            <div
+              className="my-1 text-slate-300 text-sm md:text-lg font-thin tracking-wider"
+              dangerouslySetInnerHTML={{ __html: cold }}
+            />
+          ) : (
+            <ReactQuill
+              className="text-slate-300"
+              value={sessionText}
+              onChange={handleChange}
+            />
+          )}
+        </section>
+        <div className="w-4/5 h-px bg-gray-200 my-2 mx-2 opacity-50" />
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 };
 
 export default function SessionList({ id, sessions, hot, title }) {
   const [open, setOpen] = useState(false);
-  const width = useRef(window.innerWidth);
-  const size = width.current < 768 ? 16 : 24;
+
   if (!sessions.length) {
     return null;
   }
@@ -78,16 +104,22 @@ export default function SessionList({ id, sessions, hot, title }) {
       open={open}
       onOpenChange={setOpen}
     >
-      <div className="border border-slate-400 rounded my-[10px] p-[10px] flex justify-between">
-        <p className="text-s md:text-lg text-slate-300 font-thin tracking-wider">
+      <div className=" my-[10px] p-[10px] pl-0 flex justify-start">
+        <p className="mr-2 text-s md:text-lg text-slate-300 font-thin tracking-wider">
           Past Sessions
         </p>
         <Collapsible.Trigger asChild>
           <button id={open ? "close" : "open"}>
             {open ? (
-              <XCircle className="p-0.5 text-slate-300  hover:text-slate-600" />
+              <XCircle
+                className="p-0.5 text-slate-300  hover:text-slate-600"
+                strokeWidth={1}
+              />
             ) : (
-              <ArrowDownFromLine className="p-0.5 text-slate-300 hover:text-slate-600" />
+              <ArrowDownFromLine
+                className="p-0.5 text-slate-300 hover:text-slate-600"
+                strokeWidth={1}
+              />
             )}
           </button>
         </Collapsible.Trigger>
